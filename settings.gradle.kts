197c86ea0f080e7dfafd90e70a3130f9183da1f3
Paul Merlin <paul@gradle.com>
2025-05-27 17:17:21 +0200

    Move repositories declaration to settings.gradle.kts and filter content for performance

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,22 @@
 rootProject.name = "spring-petclinic-kotlin"
 
 pluginManagement {
-	repositories {
-		maven { url = uri("https://repo.spring.io/snapshot") }
-		maven { url = uri("https://repo.spring.io/milestone") }
-		gradlePluginPortal()
-	}
+    includeBuild("build-logic")
+    repositories {
+        maven {
+            url = uri("https://repo.spring.io/milestone")
+            content { includeGroupByRegex("org\\.springframework\\..*") }
+        }
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        maven {
+            url = uri("https://repo.spring.io/milestone")
+            content { includeGroupByRegex("org\\.springframework\\..*") }
+        }
+        mavenCentral()
+    }
 }
